bb2e6468863dfa944271bb3503428c29df260e37
bobonYa <st.anger_nov@mail.ru>
2015-02-20 23:48:58 +0600

    2

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var http = require('http');
 http.createServer(function(req, res) {
 	console.log("Server start");
 	//res.statusCode = 200;
-	res.header ('Access-Control-Allow-Origin', '*');
+	res.setHeader ('Access-Control-Allow-Origin', '*');
+	res.statusCode = 200;
 	res.end("NodeJs server create");
 
 
