f7b9ebc1978815fd8173fd85a347f1c5d8e49898
bobonYa <st.anger_nov@mail.ru>
2015-02-20 23:36:05 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+
+http.createServer(function(req, res) {
+	console.log("Server start");
+	res.statusCode = 200;
+	res.end("NodeJs server create");
+
+
+}).listen(1400);
