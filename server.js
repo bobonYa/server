38df49f702ef01fd97b114fb5a031e135a4be5bc
bobonYa <st.anger_nov@mail.ru>
2015-02-20 23:43:33 +0600

    test cross

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var http = require('http');
 
 http.createServer(function(req, res) {
 	console.log("Server start");
-	res.statusCode = 200;
+	//res.statusCode = 200;
+	res.writeHead(200, {"Control-Allow-Origin": "*"});
 	res.end("NodeJs server create");
 
 
